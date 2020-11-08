6799bc8244140e7f09fe9670ae10e3242c258bed
Randika Rodrigo <subash.rodrigo@gmail.com>
2020-11-08 12:31:26 +0530

    Added gravatar library and user register route implementation.

--- models/User.js
+++ models/User.js
@@ -4,8 +4,8 @@ const Schema = mongoose.Schema;
 const UserSchema = new Schema({
 	name: { type: String, required: true },
 	email: { type: String, required: true },
-	passport: { type: String, required: true },
-	avatar: { type: String, required: true },
+	password: { type: String, required: true },
+	avatar: { type: String },
 	date: { type: Date, default: Date.now }
 });
 
--- routes/api/users.js
+++ routes/api/users.js
@@ -1,9 +1,47 @@
 const express = require('express');
+const gravatar = require('gravatar');
+const bcrypt = require('bcryptjs');
 const router = express.Router();
 
-router.get('/', (req, res) => {
-	res.status(200).send({
-		message: 'Users works'
+// load user model
+const User = require('../../models/User');
+
+router.post('/register', (req, res) => {
+	User.findOne({ email: req.body.email }).then((user) => {
+		if (user) {
+			return res.status(400).json({ email: 'Email already taken' });
+		} else {
+			const avatar = gravatar.url(req.body.email, {
+				s: '200', // size
+				r: 'pg', // rating
+				d: 'mm' // default
+			});
+
+			const newUser = new User({
+				name: req.body.name,
+				email: req.body.email,
+				avatar,
+				password: req.body.password
+			});
+
+			bcrypt.genSalt(10, (err, salt) => {
+				bcrypt.hash(newUser.password, salt, (err, hash) => {
+					if (err) {
+						res.status(500).json({ password: 'cannot perform hashing' });
+					}
+
+					newUser.password = hash;
+					newUser
+						.save()
+						.then((user) => {
+							res.status(201).json(user);
+						})
+						.catch((err) => {
+							console.error(err);
+						});
+				});
+			});
+		}
 	});
 });
 
