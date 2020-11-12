4e3b88d9997a30ba7368ee83b205386aa4f39ba5
Randika Rodrigo <subash.rodrigo@gmail.com>
2020-11-12 22:41:22 +0530

    Added like and unlike the post routes and altered few error messages in delete post route.

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -56,34 +56,124 @@ router.delete(
 	'/:id',
 	passport.authenticate('jwt', { session: false }),
 	(req, res) => {
-		Profile.findOne({ user: req.user.id }).then((profile) => {
-			Post.findById(req.params.id).then((post) => {
-				// check whether post is already exists
-				if (!post) {
-					return res.status(404).json({ post: 'No post found' });
-				}
+		Profile.findOne({ user: req.user.id })
+			.then((profile) => {
+				Post.findById(req.params.id)
+					.then((post) => {
+						// check whether post is already exists
+						if (!post) {
+							return res
+								.status(400)
+								.json({ post: 'Cannot delete already deleted post' });
+						}
 
-				// check whether post owner is going to delete this post
-				if (post.user.toString() !== req.user.id) {
-					return res
-						.status(401)
-						.json({ authorization: 'User not authorized to delete the post' });
-				}
+						// check whether post owner is going to delete this post
+						if (post.user.toString() !== req.user.id) {
+							return res.status(401).json({
+								authorization: 'User not authorized to delete the post'
+							});
+						}
 
-				// remove post
-				post
-					.remove()
-					.then(() =>
-						res.status(200).json({
-							success: true,
-							message: 'Post successfully deleted.'
-						})
-					)
+						// remove post
+						post
+							.remove()
+							.then(() =>
+								res.status(200).json({
+									success: true,
+									message: 'Post successfully deleted.'
+								})
+							)
+							.catch((err) =>
+								res.status(400).json({ post: 'Unable to delete the post' })
+							);
+					})
 					.catch((err) =>
-						res.status(404).json({ post: 'No post found to delete' })
+						res.status(400).json({ post: 'No post found with this id' })
 					);
-			});
-		});
+			})
+			.catch((err) =>
+				res.status(400).json({ post: 'Unable to delete the post' })
+			);
+	}
+);
+
+// like the post
+router.post(
+	'/:id/like',
+	passport.authenticate('jwt', { session: false }),
+	(req, res) => {
+		Profile.findOne({ user: req.user.id })
+			.then((profile) => {
+				Post.findById(req.params.id)
+					.then((post) => {
+						// check if user already liked the post
+						if (
+							post.likes.some((like) => like.user.toString() === req.user.id)
+						) {
+							return res
+								.status(400)
+								.json({ like: 'User already liked this post' });
+						}
+
+						// save likes into the post
+						post.likes.unshift({ user: req.user.id });
+						post
+							.save()
+							.then((post) => res.status(200).json(post))
+							.catch((err) =>
+								res.status(400).json({ like: 'Unable to like the post' })
+							);
+					})
+					.catch((err) =>
+						res.status(404).json({ post: 'No post found with this id' })
+					);
+			})
+			.catch((err) =>
+				res.status(400).json({ post: 'Unable to like the post' })
+			);
+	}
+);
+
+// dislike the post
+router.post(
+	'/:id/unlike',
+	passport.authenticate('jwt', { session: false }),
+	(req, res) => {
+		Profile.findOne({ user: req.user.id })
+			.then((profile) => {
+				Post.findById(req.params.id)
+					.then((post) => {
+						// check if user already liked the post
+						if (
+							!post.likes.some((like) => like.user.toString() === req.user.id)
+						) {
+							return res
+								.status(400)
+								.json({ unlike: 'You have not like this post yet' });
+						}
+
+						// get the remove index of likes list
+						const removeLikeIndex = post.likes.findIndex(
+							(like) => like.user.toString() === req.user.id
+						);
+
+						// remove from likes list
+						post.likes.splice(removeLikeIndex, 1);
+
+						post
+							.save()
+							.then((post) => res.status(200).json(post))
+							.catch((err) =>
+								res.status(400).json({ like: 'Unable to unlike the post' })
+							);
+					})
+					.catch((err) =>
+						res.status(404).json({ post: 'No post found with this id' })
+					);
+			})
+			.catch((err) =>
+				res.status(400).json({ post: 'Unable to unlike the post' })
+			);
 	}
 );
 
