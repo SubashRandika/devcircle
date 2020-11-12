c80bc4217bbf4fa582ca970a91e28f7ac5cfc0d2
Randika Rodrigo <subash.rodrigo@gmail.com>
2020-11-12 21:39:03 +0530

    Added a route for delete a post by id by owner who posted.

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -6,6 +6,7 @@ const validatePostInput = require('../../validation/post');
 
 // get post model from
 const Post = require('../../models/Post');
+const Profile = require('../../models/Profile');
 
 // get all posts
 router.get('/', (req, res) => {
@@ -47,6 +48,42 @@ router.post(
 			.save()
 			.then((post) => res.status(200).json(post))
 			.catch((err) => res.status(400).json(err));
+	}
+);
+
+// delete a post
+router.delete(
+	'/:id',
+	passport.authenticate('jwt', { session: false }),
+	(req, res) => {
+		Profile.findOne({ user: req.user.id }).then((profile) => {
+			Post.findById(req.params.id).then((post) => {
+				// check whether post is already exists
+				if (!post) {
+					return res.status(404).json({ post: 'No post found' });
+				}
+
+				// check whether post owner is going to delete this post
+				if (post.user.toString() !== req.user.id) {
+					return res
+						.status(401)
+						.json({ authorization: 'User not authorized to delete the post' });
+				}
+
+				// remove post
+				post
+					.remove()
+					.then(() =>
+						res.status(200).json({
+							success: true,
+							message: 'Post successfully deleted.'
+						})
+					)
+					.catch((err) =>
+						res.status(404).json({ post: 'No post found to delete' })
+					);
+			});
+		});
 	}
 );
 
