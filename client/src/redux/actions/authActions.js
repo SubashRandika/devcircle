502636b5c6f99c0400c21f59d77a217d3ce7966e
Randika Rodrigo <subash.rodrigo@gmail.com>
2020-12-01 23:35:27 +0530

    Added jwt-decode to extract signin user data from token and implemented user login reducer functionality with empty check utility.

--- client/src/redux/actions/authActions.js
+++ client/src/redux/actions/authActions.js
@@ -1,5 +1,7 @@
 import axios from 'axios';
-import { GET_ERRORS } from '../constants/authTypes';
+import jwt_decode from 'jwt-decode';
+import setAuthToken from '../../utils/setAuthToken';
+import { GET_ERRORS, SET_CURRENT_USER } from '../constants/authTypes';
 
 // Register user (SignUp user)
 export const registerUser = (userData, history) => (dispatch) => {
@@ -15,4 +17,38 @@ export const registerUser = (userData, history) => (dispatch) => {
 				payload: err.response.data
 			});
 		});
+};
+
+// Login user (SignIn user)
+export const loginUser = (credentials) => (dispatch) => {
+	axios
+		.post('/api/users/login', credentials)
+		.then((res) => {
+			const { token } = res.data;
+
+			// Save to local storage
+			localStorage.setItem('jwtToken', token);
+
+			// set authorization header with jwt token
+			setAuthToken(token);
+
+			// decode jwt token to extract login user data
+			const decodedUserData = jwt_decode(token);
+
+			// set current login user
+			dispatch(setCurrentUser(decodedUserData));
+		})
+		.catch((err) => {
+			dispatch({
+				type: GET_ERRORS,
+				payload: err.response.data
+			});
+		});
+};
+
+export const setCurrentUser = (userData) => {
+	return {
+		type: SET_CURRENT_USER,
+		payload: userData
+	};
 };
