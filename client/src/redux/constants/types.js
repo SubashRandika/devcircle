983daf039f05083d8038554507261f1d4062c54a
Randika Rodrigo <subash.rodrigo@gmail.com>
2021-03-20 14:12:16 +0530

    Removed post object from post reducer and action type of get a individual post.

--- client/src/redux/constants/types.js
+++ client/src/redux/constants/types.js
@@ -16,7 +16,6 @@ export const CLEAR_CURRENT_PROFILE = 'CLEAR_CURRENT_PROFILE';
 // post types
 export const CREATE_POST = 'CREATE_POST';
 export const POST_LOADING = 'POST_LOADING';
-export const GET_A_POST = 'GET_A_POST';
 export const GET_ALL_POSTS = 'GET_ALL_POSTS';
 export const REMOVE_POST = 'REMOVE_POST';
 export const LIKE_DISLIKE_POST = 'LIKE_DISLIKE_POST';
--- client/src/redux/reducers/postReducer.js
+++ client/src/redux/reducers/postReducer.js
@@ -8,7 +8,6 @@ import {
 
 const initialState = {
 	posts: [],
-	post: {},
 	loading: false
 };
 
